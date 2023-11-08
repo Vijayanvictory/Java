03ea5b0c1fa7d933ead6b16fe89ba2f8fd99da27
Vijayanvictory <p.vijan06@gmail.com>
2023-11-08 10:13:08 +0530

    java block level

---
+++ blockLevel.java
@@ -0,0 +1,65 @@
+class blockLevel{
+	void userData()
+		{
+		int a = 100; 
+			{
+			int b = 50;
+			System.out.println(a+b);
+			}
+		}
+	public static void main(String args[])
+	{
+	blockLevel bl = new blockLevel();
+		bl.userData();
+	}
+
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
